b9a226a6dd777d9da7516283af2cd804501da54e
Matheus Rubi <matheusrubisch@gmail.com>
2021-11-04 23:22:33 -0300

    refactor: cors

--- BackEnd/src/api/app.js
+++ BackEnd/src/api/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 require('dotenv').config();
 
 const err = require('../middlewares/err');
@@ -10,6 +11,7 @@ const app = express();
 
 const PORT = process.env.PORT;
 
+app.use(cors());
 app.use(express.json());
 app.use('/users', users);
 app.use('/login', login);
