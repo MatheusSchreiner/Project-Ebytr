c0c7881ce36954a4d8da4ac5141f3be9aab7d600
Matheus Rubi <matheusrubisch@gmail.com>
2021-11-05 00:47:53 -0300

    refactor: app, mensagem de bemvindo

--- BackEnd/src/api/app.js
+++ BackEnd/src/api/app.js
@@ -16,6 +16,7 @@ app.use(express.json());
 app.use('/users', users);
 app.use('/login', login);
 app.use('/tasks', tasks);
+app.get('/', (_req, res) => res.status(200).json({ message: 'Seja Bem-vindo' }));
 app.use(err);
 
 app.listen(PORT, console.log(`conectado na porta: ${PORT}`));
